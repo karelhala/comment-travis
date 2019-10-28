71b332ac244291092351234725e2ac6a7475e8f1
Karel Hala <karelhala.91@gmail.com>
2019-10-28 10:55:04 +0100

    Do not include package folders

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { createComment, bot } = require('./comment-bot.js');
 
-const { trigger: configTrigger, releaseMapper: configReleaseMapper, users, monorepo } = require('./config.json');
+const { trigger: configTrigger, releaseMapper: configReleaseMapper, users } = require('./config.json');
 
 const trigger = configTrigger || 'release';
 
@@ -8,10 +8,6 @@ const releaseMapper = configReleaseMapper || {
   major: 'major',
   minor: 'minor',
   bugfix: 'bugfix'
-}
-
-function calculatePackages(files) {
-  return [...new Set(files.map(({ filename }) => `${filename.substring(monorepo.length + 1).split('/')[0]}/`))];
 }
 
 /**
@@ -35,18 +31,9 @@ module.exports = app => {
         if (pullRequest.merged) {
           app.log('PR has been merged, let\'s release!');
           const type = releaseMapper[corrected.substring(trigger.length)] || 'bugfix';
-          const { data: files } = await context.github.pullRequests.listFiles(currPr);
-          const packageFolders = monorepo ? calculatePackages(files) : ['/'];
           context.log(`We will trigger new Release: ${type}!`);
-          if (monorepo) {
-            createComment({ ...currPr, body: `We will trigger new Release: ${type}!\nFor package folders:\n * ${
-              calculatePackages(packageFolders).join('\n * ')
-            }` }, context);
-          } else {
-            createComment({ ...currPr, body: `We will trigger new Release: ${type}!` }, context);
-          }
+          createComment({ ...currPr, body: `We will trigger new Release: ${type}!` }, context);
         }
-
         createComment({ ...currPr, body: 'Sorry no release, PR has not been merged.' }, context);
       }
     }
