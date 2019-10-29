82e8ed8d9fd424a905f463685287f9dbfa66c311
Karel Hala <khala@redhat.com>
2019-10-29 15:53:02 +0100

    Allow trigger with label

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { createComment } = require('./comment-bot');
-const { trigger: configTrigger, releaseMapper: configReleaseMapper, users } = require('./config.json');
+const {
+    trigger: configTrigger,
+    releaseMapper: configReleaseMapper,
+    users,
+    labels: configLabels
+} = require('./config.json');
 const travisTrigger = require('./travis-bot');
 
 const trigger = configTrigger || 'release';
@@ -10,6 +15,11 @@ const releaseMapper = configReleaseMapper || {
     bugfix: 'bugfix'
 }
 
+const labels = configLabels || {
+    release: 'bugfix',
+    'release minor': 'minor'
+}
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
@@ -17,6 +27,27 @@ const releaseMapper = configReleaseMapper || {
 module.exports = app => {
     app.log(`Trigger word: ${trigger}`);
     app.log(`${Object.entries(releaseMapper).map(([key, value]) => `will look for - ${key} as ${value}`).join('\n')}`);
+    app.log(`Release labels: \n${Object.entries(labels).map(([key, value]) => `will look for - ${key} as ${value}`).join('\n')}`);
+
+    app.on(['pull_request.closed', 'pull_request.labeled'], async context => {
+        const currPr = context.issue();
+        context.log(currPr);
+        context.log('PR has been closed!');
+        if (context.payload.pull_request.merged) {
+            context.log('PR has been actually merged!');
+            context.log(context.payload.pull_request.labels);
+            const releaseLabel = context.payload.pull_request.labels.find(label => label.name in labels);
+            if (releaseLabel) {
+                const type = labels[releaseLabel.name];
+                context.log(`We will trigger new Release: ${type}!`);
+                createComment({ ...currPr, body: `We will trigger new Release: ${type}!` }, context);
+                return travisTrigger(currPr, type, context);
+            } else {
+                context.log(`No release label found, no release triggered.`);
+            }
+        }
+    });
+
     app.on(['issue_comment.edited', 'issue_comment.created'], async context => {
         let allowed = true;
         if (users && !users.some(user => user === context.payload.sender.login)) {
